7b7b7c483bec00acb3ca93e89af24d203586956d
Erick Jean <erickjean36@gmail.com>
2026-04-25 14:57:42 -0400

    refactor: update seed script to use HashService for password hashing

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -1,6 +1,7 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma/client";
+import 'dotenv/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../generated/prisma/client';
+import { HashService } from 'src/common/hash/hash.service';
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
@@ -8,32 +9,37 @@ const adapter = new PrismaPg({
 
 const prisma = new PrismaClient({ adapter });
 
+const hashService = new HashService();
+
 async function main() {
   const count = await prisma.user.count();
 
   if (count > 0) {
-    console.log("Seed ignorado: já existem usuários");
+    console.log('Seed ignorado: já existem usuários');
     return;
   }
+
+  const adminPassword = await hashService.hash('123456');
+  const userPassword = await hashService.hash('123456');
 
   await prisma.user.createMany({
     data: [
       {
-        name: "Admin",
-        email: "admin@email.com",
-        password: "123456",
-        role: "admin",
+        name: 'Admin',
+        email: 'admin@email.com',
+        password: adminPassword,
+        role: 'admin',
       },
       {
-        name: "User",
-        email: "user@email.com",
-        password: "123456",
-        role: "user",
+        name: 'User',
+        email: 'user@email.com',
+        password: userPassword,
+        role: 'user',
       },
     ],
   });
 
-  console.log("Seed executado com sucesso");
+  console.log('Seed executado com sucesso');
 }
 
 main()
