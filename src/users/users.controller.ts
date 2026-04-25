3550598af89c3fc85c6161159f3bc2ec6c13d1bc
Erick Jean <erickjean36@gmail.com>
2026-04-25 15:27:15 -0400

    feat: integrate Swagger for API documentation and enhance users controller with Swagger decorators

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Listar usuários' })
   findAll() {
     return this.usersService.findAll();
   }
